52ada8b351937afb83cc3a7c3e1c1d8089d61922
aminjam <aminjam.software@gmail.com>
2015-10-24 10:35:35 -0400

    modularizing the vim plugins

--- setup.sh
+++ setup.sh
@@ -22,13 +22,38 @@ config_neovim(){
 	cp "$DIR/neovim/nvimrc" ~/.nvimrc
 }
 
+setup_go(){
+	if which go > /dev/null; then
+		echo -e "${yellow}INSTALL${end_color} ${bold}golang${normal} dependencies..."
+		gopackages="\
+		github.com/golang/lint/golint \
+		github.com/jstemmer/gotags \
+		github.com/kisielk/errcheck \
+		github.com/nsf/gocode \
+		golang.org/x/tools/cmd/godoc \
+		golang.org/x/tools/cmd/goimports \
+		golang.org/x/tools/cmd/gorename \
+		golang.org/x/tools/cmd/oracle \
+		golang.org/x/tools/cmd/vet \
+		"
+
+		for p in $gopackages; do
+			echo -e "\t${yellow}INSTALL${end_color} ${bold}${p}${normal}..."
+			go get -f -u $p
+			echo -e "\t${green}DONE${end_color}    ${bold}${p}${normal}"
+		done
+		echo -e "${green}DONE${end_color}    ${bold}golang${normal} dependencies"
+	fi
+}
+
 main(){
 	set +e
 	set -o pipefail
 	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+	setup_go
 	config_bash
 	config_git
 	config_tmux
-  config_neovim
+	config_neovim
 }
 main "$@"
