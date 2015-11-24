97d6ff410d887aea4f70f21472e5f36734d0b83f
Amin Jams <aminjam.software@gmail.com>
2015-11-23 22:07:55 -0500

    cleanup
    * Add bash configuration
    * Remove neovim in favor of luan/vimfiles
    * Add .gitmesssage

--- setup.sh
+++ setup.sh
@@ -7,43 +7,13 @@ config_tmux(){
 }
 config_git(){
 	cp "$DIR/git/gitconfig" ~/.gitconfig
+	cp "$DIR/git/gitmessage" ~/.gitmessage
 }
 config_bash(){
 	curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 	curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
 	cp "$DIR/bash/bashrc" ~/.bashrc
 	cp "$DIR/bash/bash_profile" ~/.bash_profile
-}
-
-config_neovim(){
-	rm -rf ~/.nvim
-	curl -fLo ~/.nvim/autoload/plug.vim --create-dirs \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	cp "$DIR/neovim/nvimrc" ~/.nvimrc
-}
-
-setup_go(){
-	if which go > /dev/null; then
-		echo -e "${yellow}INSTALL${end_color} ${bold}golang${normal} dependencies..."
-		gopackages="\
-		github.com/golang/lint/golint \
-		github.com/jstemmer/gotags \
-		github.com/kisielk/errcheck \
-		github.com/nsf/gocode \
-		golang.org/x/tools/cmd/godoc \
-		golang.org/x/tools/cmd/goimports \
-		golang.org/x/tools/cmd/gorename \
-		golang.org/x/tools/cmd/oracle \
-		golang.org/x/tools/cmd/vet \
-		"
-
-		for p in $gopackages; do
-			echo -e "\t${yellow}INSTALL${end_color} ${bold}${p}${normal}..."
-			go get -f -u $p
-			echo -e "\t${green}DONE${end_color}    ${bold}${p}${normal}"
-		done
-		echo -e "${green}DONE${end_color}    ${bold}golang${normal} dependencies"
-	fi
 }
 
 main(){
